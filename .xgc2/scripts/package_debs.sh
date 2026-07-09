a8a2f62a0b127cd259f6b67f7a27aa5c6719ab5f
Lxk_buaa <867768510@qq.com>
2026-07-10 03:55:37 +0800

    Bump Scout Gazebo package version

--- .xgc2/scripts/package_debs.sh
+++ .xgc2/scripts/package_debs.sh
@@ -138,7 +138,7 @@ build_deb() {
 }
 
 gazebo_pkg="ros-noetic-xgc2-gazebo-sim-scout"
-description_dep="ros-noetic-xgc2-scout-description (>= 0.4.10-1)"
+description_dep="ros-noetic-xgc2-scout-description (>= 0.4.10-2)"
 
 build_deb \
   "${gazebo_pkg}" \
