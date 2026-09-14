6ec28fdc391c4ab8df22f4c564bbe7fd69994630
Xiaokang Lü <141200314+lxk36@users.noreply.github.com>
2026-09-14 15:44:59 +0800

    fix(scout): use a simulation-clock command dispatch timer

--- scout/include/scout_gazebo/scout_skid_steer.hpp
+++ scout/include/scout_gazebo/scout_skid_steer.hpp
@@ -17,6 +17,7 @@
 
 #include "xgc_chassis_hold/udp.hpp"
 #include "scout_gazebo/command_delay.hpp"
+#include "scout_gazebo/wheel_allocation.hpp"
 
 namespace wescore {
 class ScoutSkidSteer {
@@ -39,11 +40,13 @@ private:
   double command_gain_;
   double angular_command_gain_;
   double command_delay_s_;
+  double command_dispatch_period_s_;
   bool enable_command_limits_;
   double max_linear_speed_;
   double max_angular_speed_;
 
   CommandDelay command_delay_;
+  WheelAllocation wheel_allocation_;
 
   ros::NodeHandle *nh_;
 
@@ -51,15 +54,16 @@ private:
   ros::Publisher motor_fl_pub_;
   ros::Publisher motor_rl_pub_;
   ros::Publisher motor_rr_pub_;
+  ros::Publisher dispatch_trace_pub_;
 
   ros::Subscriber cmd_sub_;
-  ros::WallTimer control_timer_;
+  ros::Timer control_timer_;
   xgc_chassis_hold::Gate hold_gate_;
 
   void TwistCmdCallback(const geometry_msgs::Twist::ConstPtr &msg);
   void PublishZeroMotors();
   static void HoldZeroThunk(void *self);
-  void ControlTick(const ros::WallTimerEvent &event);
+  void ControlTick(const ros::TimerEvent &event);
   double Clamp(double value, double limit) const;
   std::string JoinTopic(const std::string &ns, const std::string &topic) const;
 };
